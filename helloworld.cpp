e6519a2e143cb6fc9ed8aed4eebfbfb864ae3ec8
Nate-OBrien <159456288+Nate-OBrien@users.noreply.github.com>
2024-09-10 10:34:26 -0400

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int number = 5;
+float decimal = 5.2;
+char character = 'a';
+bool tOrF = true;
+std::string line = "Test line!";
+
+int main()
+{
+    std::cout << number << std::endl;
+    std::cout << decimal << std::endl;
+    std::cout << character << std::endl;
+    std::cout << tOrF << std::endl;
+    std::cout << line << std::endl;
+}
